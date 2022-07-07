05a195e82ffb83710c48897779b4a57ea35c5557
Tatianayarchenko <ktanya61@gmail.com>
2022-07-07 23:33:12 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Modal } from 'components/Modal';
+
+export const App = () => {
+  return (
+    <div>
+      <Modal />
+    </div>
+  );
+};
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,9 @@
+import { Backdrop, ModalContent } from 'components/Modal/Modal.styled';
+
+export const Modal = () => (
+  <Backdrop>
+    <ModalContent>
+      <img src="" alt="" />
+    </ModalContent>
+  </Backdrop>
+);
