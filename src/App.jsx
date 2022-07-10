f2e518f035724574c5d9dd08809b274a072ba075
Tatianayarchenko <ktanya61@gmail.com>
2022-07-10 16:16:26 +0300

    add modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import { Modal } from 'components/Modal';
+import { Component } from 'react';
 
-export const App = () => {
-  return (
-    <div>
-      <Modal />
-    </div>
-  );
-};
+export class App extends Component {
+  state = {
+    showModal: false,
+  };
+
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={this.toggleModal}>
+          Открыть модалку
+        </button>
+        {this.state.showModal && (
+          <Modal onClose={this.toggleModal}>
+            <button type="button" onClick={this.toggleModal}>
+              Закрыть модалку
+            </button>
+          </Modal>
+        )}
+      </div>
+    );
+  }
+}
